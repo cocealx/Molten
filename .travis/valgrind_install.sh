2f953ec38166a70a6b3d7c673ad086c20b6de39e
xuming <1975178521@qq.com>
2020-01-10 15:18:33 +0800

    m

--- .travis/valgrind_install.sh
+++ .travis/valgrind_install.sh
@@ -3,9 +3,8 @@ function logit(){
     echo "[php_build] $@" 1>&2
 }
 #main
-PATH_VALGRIND=$1
-cd $PATH_VALGRIND
-wget http://www.valgrind.org/downloads/valgrind-3.14.0.tar.bz2 && tar -xjvf valgrind-3.14.0.tar.bz2 && cd valgrind-3.14.0/ && ./configure && make &&make install
+PATH_VALGRIND_BIN=$1
+wget http://www.valgrind.org/downloads/valgrind-3.14.0.tar.bz2 && tar -xjvf valgrind-3.14.0.tar.bz2 && cd valgrind-3.14.0/ && ./configure --prefix=$PATH_VALGRIND_BIN && make &&make install
 
 ret=$?
 
